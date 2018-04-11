9d8ed32e10b7a24fed74f8afe6b3dd275603a16d
Lakshmi K <lakshmi.k@flex.com>
2018-04-10 22:30:58 -0700

    [Studio] Add screen "preworkday"

---
+++ client/controllers/app/preworkday.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_preworkday', app_preworkday);
+function app_preworkday($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.preworkday', {
+        views: {
+            app: {
+                controller: 'app_preworkday',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.preworkday');
+                }
+            }
+        }
     });
 });
